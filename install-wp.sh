696dbee5c2cf81bc6e48641c8b2c1df580538842
Mahmoud <mahmoud.farhadi91@gmail.com>
2023-08-16 20:44:55 +0330

    updated

--- install-wp.sh
+++ install-wp.sh
@@ -12,11 +12,24 @@ DB_PASS=${DB_PASS:-$(openssl rand -base64 12)}
 read -p "Enter the database name (default: wp_database): " DB_NAME
 DB_NAME=${DB_NAME:-wp_database}
 
-# Rest of the script remains unchanged
-WP_DIR="/var/www/html/"
+# Specify the WordPress folder location
+WP_DIR="/var/www/html/wp"
 WP_URL="https://wordpress.org/latest.zip"
 
-# Create WordPress directory
+# Check if the database and user exist and remove them
+EXIST_DB=$(mysql -u root -p -e "SELECT COUNT(*) FROM information_schema.SCHEMATA WHERE SCHEMA_NAME='$DB_NAME';" | grep -v COUNT)
+if [ "$EXIST_DB" -eq 1 ]; then
+    echo "Removing existing database $DB_NAME..."
+    mysql -u root -p -e "DROP DATABASE $DB_NAME;"
+fi
+
+EXIST_USER=$(mysql -u root -p -e "SELECT COUNT(*) FROM mysql.user WHERE user = '$DB_USER';" | grep -v COUNT)
+if [ "$EXIST_USER" -eq 1 ]; then
+    echo "Removing existing user $DB_USER..."
+    mysql -u root -p -e "DROP USER '$DB_USER'@'localhost';"
+fi
+
+# Create WordPress subfolder
 sudo mkdir -p $WP_DIR
 sudo chown -R www-data:www-data $WP_DIR
 cd $WP_DIR
@@ -43,11 +56,19 @@ sudo find . -type d -exec chmod 755 {} \;
 sudo find . -type f -exec chmod 644 {} \;
 sudo chown -R www-data:www-data .
 
-# Display instructions for the user
-echo "WordPress installation is complete!"
+# Update Apache configuration to point to the WordPress folder
+sudo sed -i "s|DocumentRoot /var/www/html/example|DocumentRoot $WP_DIR|" /etc/apache2/sites-available/000-default.conf
+sudo sed -i "s|<Directory '/var/www/html/example'>|<Directory '$WP_DIR'>|" /etc/apache2/sites-available/000-default.conf
+
+# Restart Apache
+sudo systemctl restart apache2
+
+# Display database information to the user
+echo "WordPress installation in subfolder is complete!"
 echo "Database Name: $DB_NAME"
 echo "Database User: $DB_USER"
 echo "Database Password: $DB_PASS"
-echo "Please finish the setup by visiting: ${ivp4}"
+echo "Please finish the setup by visiting: http://${ivp4}"
 
-
+# Clean up
+sudo apt-get clean
